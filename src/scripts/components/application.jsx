fc0b9ec9bae1add1fc42eb55788197fb3192d653
Vincent <ghzmdr@gmail.com>
2016-04-28 02:37:01 +0200

    Initial render optimizations

--- src/scripts/components/application.jsx
+++ src/scripts/components/application.jsx
@@ -4,8 +4,7 @@ import Background from './background'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 export default class Application extends React.Component {
-
-	render() {
+  render() {
 		return <div id="Application">
             {/* <Menu /> */}
             <Background />
--- src/scripts/components/background.jsx
+++ src/scripts/components/background.jsx
@@ -6,7 +6,6 @@ import polkaSketch from '../../assets/sketches/polka'
 export default class Background extends React.Component {
     
     componentDidMount() {
-        console.log(this.refs.background)
         this.setState({
             sketch: new p5(polkaSketch(p5), this.refs.background)
         })
